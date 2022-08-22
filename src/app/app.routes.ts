95b279b7e271f153b0d1dd71345d6407946d4986
Brejcha13320 <davidmejia-2000@outlook.com>
2022-08-22 12:07:23 -0500

    useHash en routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,13 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 
-
 const APP_ROUTES: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'heroes', component: HeroesComponent },
-    { path: 'heroe/:id', component: HeroeComponent },
-    { path: 'buscador/:termino', component: BuscadorComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: 'buscador/:termino', component: BuscadorComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
